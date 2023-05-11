cd16c4089dda18279478f33a4682a91a4332aad5
LeonardoDevBack <leonardo.goncalves@reltec.com.br>
2023-05-10 21:31:03 -0300

    init project c# demo traces

---
+++ ProjectInputData/Program.cs
@@ -0,0 +1,52 @@
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Extensions.Hosting;
+using Examples.Console;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var serviceName = "MyCompany.MyProduct.MyService";
+var serviceVersion = "1.0.0";
+
+builder.Services.AddOpenTelemetry()
+  .WithTracing(b =>
+  {
+      b
+      .AddOtlpExporter(a => a.Endpoint = new Uri("http://host.docker.internal:4317"))
+      .AddSource(serviceName)
+      .ConfigureResource(resource =>
+          resource.AddService(
+            serviceName: serviceName,
+            serviceVersion: serviceVersion));
+  });
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/forceLogging", () =>
+{
+    TestOtlpExporter.Run("http://localhost:4317", "grpc");
+    return "teste ok";
+})
+.WithName("Observability Teste")
+.WithOpenApi();
+
+
+app.Run();
