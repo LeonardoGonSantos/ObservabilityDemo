b9299ad8eb7110b335a38ffeced9f00f5a89fe04
LeonardoDevBack <leonardo.goncalves@reltec.com.br>
2023-05-17 15:52:14 -0300

    init new test data input

---
+++ ProjectInputData2/ProjectInputData2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics.Metrics;
+using System.Diagnostics;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Extensions.Hosting;
+using OpenTelemetry.Instrumentation;
+using OpenTelemetry.Exporter.OpenTelemetryProtocol;
+using OpenTelemetry.Exporter;
+
+// This is required if the collector doesn't expose an https endpoint. By default, .NET
+// only allows http2 (required for gRPC) to secure endpoints.
+AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure metrics
+builder.Services.AddOpenTelemetryMetrics(builder =>
+{
+    builder.AddHttpClientInstrumentation();
+    builder.AddAspNetCoreInstrumentation();
+    builder.AddMeter("MyApplicationMetrics");
+    builder.AddRuntimeInstrumentation();
+    builder.AddProcessInstrumentation();
+    builder.AddOtlpExporter(options => options.Endpoint = new Uri("http://localhost:4315"));
+});
+
+// Configure tracing
+builder.Services.AddOpenTelemetryTracing(builder =>
+{
+    builder.AddHttpClientInstrumentation();
+    builder.AddAspNetCoreInstrumentation();
+    builder.AddSource("MyApplicationActivitySource");
+    builder.AddOtlpExporter(options => options.Endpoint = new Uri("http://localhost:4315"));
+});
+
+// Configure logging
+builder.Logging.AddOpenTelemetry(builder =>
+{
+    builder.IncludeFormattedMessage = true;
+    builder.IncludeScopes = true;
+    builder.ParseStateValues = true;
+    builder.AddOtlpExporter(options => options.Endpoint = new Uri("http://localhost:4315"));
+});
+
+var app = builder.Build();
+
+// Create a route (GET /) that will make an http call, increment a metric and log a trace
+var activitySource = new ActivitySource("MyApplicationActivitySource");
+var meter = new Meter("MyApplicationMetrics");
+var requestCounter = meter.CreateCounter<int>("compute_requests");
+var httpClient = new HttpClient();
+
+// Routes are tracked by AddAspNetCoreInstrumentation
+// note: You can add more data to the Activity by using HttpContext.Activity.SetTag("key", "value")
+app.MapGet("/", async (ILogger<Program> logger) =>
+{
+    requestCounter.Add(1);
+
+    using (var activity = activitySource.StartActivity("Get data"))
+    {
+        // Add data the the activity
+        // You can see these data in Zipkin
+        activity?.AddTag("sample", "value");
+
+        // Http calls are tracked by AddHttpClientInstrumentation
+        var str1 = await httpClient.GetStringAsync("https://example.com");
+        var str2 = await httpClient.GetStringAsync("https://www.meziantou.net");
+
+        logger.LogInformation("Response1 length: {Length}", str1.Length);
+        logger.LogInformation("Response2 length: {Length}", str2.Length);
+    }
+
+    return Results.Ok();
+});
+
+app.Run();
